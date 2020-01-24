55baf91d78c721108575e9afac820630b05b7e2e
apple <apple@apples-MacBook-Pro-2.local>
2020-01-24 09:37:47 -0800

    periodic save

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Card from './Card';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+const [data, setData] = useState([])
+useEffect(() => {
+  axios.get('https://swapi.co/api/people/')
+  .then(response => {
+    setData(response.data.results)
+    // console.log(response.data.results);
+  })
+  .catch(error => {
+    console.log('Data not returned', error);
+  })
+}, [])
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Card data={data}/>
     </div>
   );
 }
---
+++ starwars/src/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+const Card = (data) => {
+    console.log(props.data);
+return (
+    <div>card
+        <div>
+            {props.data.map( p => 
+            <App key={data.name}> />
+        )
+        </div>
+    </div>
+
+)
+};
+
+export default Card;
